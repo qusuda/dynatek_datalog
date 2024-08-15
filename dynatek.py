757a4f686c6d79bf68d19faddba73957fe949abe
Anders Esbensen <anders@esbensen.nu>
2024-08-15 22:34:51 +0200

    File select an thread life time management

--- dynatek.py
+++ dynatek.py
@@ -2,6 +2,8 @@ import sys
 
 import download as dynalog
 import plot as dynaplot
+#import pyplot as dynapyplot
+import plot_pyqtgraph as dynapyplot
 
 from PyQt6.QtCore import Qt, QObject, pyqtSignal, QThread, QSize
 
@@ -10,6 +12,7 @@ from PyQt6.QtCore import Qt, QObject, pyqtSignal, QThread, QSize
 # from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication,
+    QFileDialog,
     QCheckBox,
     QComboBox,
     QDateEdit,
@@ -32,39 +35,30 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-# class Worker(QObject):
-#     update_progress = pyqtSignal(int)
-
-#     def do_work(self):
-#         #dynalog.download(self.line_edit_com_port.text(), self.line_edit_event.text(), self.progress_callback)
-#         dynalog.download("COM4", "Mosten", self.progress_callback)
-
-#         # total_tasks = 100
-#         # for i in range(total_tasks):
-
-#         #     time.sleep(0.1)
-
-#     def progress_callback(self, state, percentage):
-#         """v"""
-#         print(state)
-#         print(percentage)
-#         self.update_progress.emit(state, percentage)
 
 
 class Worker(QObject):
     update_progress = pyqtSignal(str, int)
-
     stop_signal = pyqtSignal()  # Signal to stop the worker
 
     def __init__(self, main_window):
         super().__init__()
         self.main_window = main_window
+        self.is_running = True
 
     def do_work(self):
-        # while not self.stop_signal.is_set():
-        self.main_window.current_file = dynalog.download(self.main_window.line_edit_com_port.text(), self.main_window.line_edit_event.text(), self.progress)
-        self.main_window.line_edit_file.setText(self.main_window.current_file)
-    
+        try:
+            self.main_window.current_file = dynalog.download(
+                self.main_window.line_edit_com_port.text(), 
+                self.main_window.line_edit_event.text(), 
+                self.progress
+            )
+            self.main_window.line_edit_file.setText(self.main_window.current_file)
+        except Exception as e:
+            self.update_progress.emit(f"Error: {str(e)}", 0)
+        finally:
+            self.is_running = False
+
     def progress(self, state, pct):
         self.update_progress.emit(state, pct)
 
@@ -104,15 +98,13 @@ class MainWindow(QMainWindow):
         event_layout = QHBoxLayout()
         event_layout.addWidget(QLabel("Event"))
         event_layout.addWidget(self.line_edit_event)
-        self.line_edit_event.setText("Mosten Spring Race")
-
+        self.line_edit_event.setText("Mosten Raceday")
 
         self.line_edit_com_port = QLineEdit()
         com_layout = QHBoxLayout()
         com_layout.addWidget(QLabel("COM port"))
         com_layout.addWidget(self.line_edit_com_port)
         self.line_edit_com_port.setText("COM4")
-
 
         self.btn_download = QPushButton()
         self.btn_download.setEnabled(True)
@@ -136,10 +128,15 @@ class MainWindow(QMainWindow):
         self.lbl_download_state = QLabel()
         self.lbl_download_state.setText("")
 
+        # Create a button to open a file and plot data
+        self.file_button = QPushButton("Select File")
+        self.file_button.clicked.connect(self.open_file_dialog)
+
         self.line_edit_file = QLineEdit()
         file_layout = QHBoxLayout()
         file_layout.addWidget(QLabel("File"))
         file_layout.addWidget(self.line_edit_file)
+        file_layout.addWidget(self.file_button)
         self.line_edit_file.setText(self.current_file)
 
         layout.addLayout(file_layout)
@@ -160,22 +157,42 @@ class MainWindow(QMainWindow):
         self.worker.update_progress.connect(self.update_progress)
 
     def update_progress(self, state, percentage):
-        #print(percentage)
         self.progress_download.setValue(percentage)
         self.lbl_download_state.setText(state)
-        if state == "Done":
+        if state == "Done" or "Error" in state:
             self.btn_download.setEnabled(True)
-            self.btn_plot.setEnabled(True)
-            self.lbl_download_state.setText("Done")
+            self.btn_plot.setEnabled(state == "Done")
             self.worker.stop_signal.emit()
 
     def download_clicked(self):
         self.btn_download.setEnabled(False)
+        # Create a new worker and thread each time a download is initiated
+        self.worker = Worker(self)
         self.worker_thread = QThread()
+
+        # Move the worker to the new thread
         self.worker.moveToThread(self.worker_thread)
+
+        # Connect signals and slots
         self.worker_thread.started.connect(self.worker.do_work)
+        self.worker.update_progress.connect(self.update_progress)
         self.worker.stop_signal.connect(self.worker_thread.quit)
+        self.worker_thread.finished.connect(self.worker_thread.deleteLater)
+        self.worker_thread.finished.connect(self.cleanup_after_download)
+
+        # Start the thread
         self.worker_thread.start()
+
+    def cleanup_after_download(self):
+        # Re-enable the button after the work is done
+        #self.btn_download.setEnabled(True)
+        self.worker_thread = None  # Reset the thread reference
+
+    def open_file_dialog(self):
+        # Open a file dialog to select a file
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Data File", "", "Log Files (*.log);;All Files (*)")
+        if file_name:
+            self.line_edit_file.setText(file_name)
 
     def plot_clicked(self):
         """v"""
@@ -184,7 +201,13 @@ class MainWindow(QMainWindow):
         #if self.current_file > 0:
         self.current_file = self.line_edit_file.text() 
         data_points = dynaplot.parse_file(self.current_file, 27, 0)
-        dynaplot.plot(data_points, self.current_file)
+        # Create a new window (QMainWindow or QWidget)
+        self.new_window = dynapyplot.PlotApp()  # You can also use QWidget()        
+        # Show the new window
+        self.new_window.show()
+        self.new_window.plot(data_points, self.current_file)
+        self.new_window.updateViews()
+
 
 
 app = QApplication(sys.argv)
